ab08ec1cf34da605cd94b88d432a0e5040c34d9d
Arafat Rahman <arafat.rahman.6003@gmail.com>
2026-08-11 17:50:11 +0600

    feat: add arithmetic operator demonstrations in oparetors.js

---
+++ Master the Basics/oparetors.js
@@ -0,0 +1,21 @@
+let a = 30;
+let b = 7;
+let devider = a / b; // 4.285714285714286
+let floor = devider.floor(); // 4
+let ceil = devider.ceil(); // 5
+let round = devider.round(); // 4
+
+let modulas = a % b; // 2
+let pow = a ^ b; // 2187
+let sqaure = Math.sqrt(a); // 5.477225575051661
+
+
+
+console.log(`modulas : ${modulas}`);
+console.log(`pow : ${pow}`);
+console.log(`sqaure : ${sqaure}`);
+console.log(`devider : ${devider}`);
+console.log(`floor : ${floor}`);
+console.log(`ceil : ${ceil}`);
+console.log(`round : ${round}`);
+
