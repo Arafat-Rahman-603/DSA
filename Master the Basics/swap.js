8a002efd989a382f7eee3312ab0b265da381c1b1
Arafat Rahman <arafat.rahman.6003@gmail.com>
2026-08-11 17:13:59 +0600

    feat: add examples for swapping variables using temporary storage and arithmetic operators

---
+++ Master the Basics/swap.js
@@ -0,0 +1,40 @@
+// Swap with extra variable
+
+function swappingwithextravariable() {
+    let a = 10;
+    let b = 20;
+    let c;
+
+    //initial values
+
+    console.log(`initial values ${a}, ${b}, ${c}`); // 10 20 undefined
+
+    //swapping
+    c = b;
+    b = a;
+    a = c;
+
+    console.log(`after swap with extra variable ${a}, ${b}`); // 20 10 20
+
+}
+
+swappingwithextravariable();
+
+
+// Swap with arithmetic operator (without extra variable)
+
+function swappingwithoperator() {
+    let a = 10;
+    let b = 20;
+    console.log(`initial values ${a}, ${b}`);
+
+    a = a + b; // 20 + 10 = 30
+    b = a - b; // 30 - 10 = 20
+    a = a - b; // 30 - 20 = 10
+
+    console.log(`after swap with operator ${a}, ${b}`); // 10, 20
+
+}
+
+swappingwithoperator();
+
