51d8af5420af62f2b59737ac3c9143c7cc1d8545
Arafat Rahman <arafat.rahman.6003@gmail.com>
2026-08-15 02:17:59 +0600

    feat: implement array operations including summation, finding max elements, reversing, and sorting binary values

--- Mastering Arrays/app.js
+++ Mastering Arrays/app.js
@@ -35,12 +35,55 @@ let max1 = arr[0];
 let max2 = arr[1];
 
 for (let i = 0; i < arr.length; i++) {
-    for (let j = 1; j < arr.length; j++) {
-        if (max1 < arr[i] || max2 < arr[j]) {
-            max1 = arr[i];
-            max2 = arr[j];
-        }
-
+    if (arr[i] > max1) {
+        max2 = max1;
+        max1 = arr[i];
+    } else if (arr[i] > max2 && arr[i] !== max1) {
+        max2 = arr[i];
     }
 }
-console.log(max)
+
+console.log(`1st max: ${max1}, 2nd max: ${max2}`);
+
+
+let reverseArrMethord1 = [];
+let arr2 = new Array(arr.length);
+
+for (let i = arr.length - 1; i >= 0; i--) {
+    reverseArrMethord1.push(arr[i])
+}
+
+let j = 0;
+for (let i = arr.length - 1; i >= 0; i--) {
+    arr2[j] = arr[i]
+    j++
+}
+
+
+    for (let i = 0; i < arr.length / 2; i++) {
+    let tmp = arr[i];
+
+    arr[i] = arr[arr.length - 1 - i];
+    arr[arr.length - 1 - i] = tmp;
+}
+
+
+console.log("reverseArrMethord1 " + reverseArrMethord1)
+console.log("arr2 " + arr2)
+console.log("arr " + arr)
+
+
+let arr3 = [1,0,1,0,1,0,0,0,1,1,1];
+
+let k =0 ;
+
+for (let i = 0; i < arr3.length; i++) {
+    if (arr3[i] === 0) {
+        let tmp = arr3[k];
+        arr3[k] = arr3[i];
+        arr3[i] = tmp;
+        k++;
+    }
+}
+
+console.log(arr3)
