78b2378e3813a7c7c682156ead6b98f80bcb4ccf
Arafat Rahman <arafat.rahman.6003@gmail.com>
2026-08-23 16:24:55 +0600

    feat: implement bubble sort and initialize selection sort structure

---
+++ Sorting Algorithms/app.js
@@ -0,0 +1,25 @@
+let arr = [1, 3, 2, 4, 6, 5];
+
+for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+        if (arr[i] > arr[j]) {
+            let tmp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = tmp;
+        }
+    }
+}
+
+console.log(arr);
+
+
+let arr2 = [2, 5, 1, 4, 3];
+let min = arr2[0];
+
+for (let i = 0; i < arr2.length; i++) {
+    for (let j = 0; j < arr2.length; j++) {
+        if (condition) {
+
+        }
+    }
+}
