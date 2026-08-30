ab10d63200af736b09335a5c9d2f2f325bd32256
Arafat Rahman <arafat.rahman.6003@gmail.com>
2026-08-30 22:54:07 +0600

    feat: add pow(x, n) solution in LeetCode

---
+++ Leet_Code/powx-n.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number} x
+ * @param {number} n
+ * @return {number}
+ */
+var myPow = function(x, n) {
+if (n === 0) return 1;
+
+    if (n < 0) {
+        x = 1 / x;
+        n = -n;
+    }
+
+    function pow(n) {
+        if (n === 0) return 1;
+
+        let half = pow(Math.floor(n / 2));
+
+        if (n % 2 === 0) {
+            return half * half;
+        } else {
+            return half * half * x;
+        }
+    }
+
+    return pow(n);
+};
